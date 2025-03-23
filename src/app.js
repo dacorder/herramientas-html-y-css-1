3956f9b3e5bc200d8b8ef2a1c0a273b7bdbb38a3
David Cordero <dacorder@uoc.edu>
2025-03-23 23:26:11 +0100

    PEC 1: Mark selected menu entry as active

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+
+// Mark as active the current page in the navbar
+const links = document.querySelectorAll("nav ul a");
+links.forEach((link) => {
+    if (link.href === window.location.href) {
+        link.classList.add("active");
+    }
+});
