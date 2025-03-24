832ceccb9d5f34b796b6d9daefb3d1848da62ad4
David Cordero <dacorder@uoc.edu>
2025-03-24 10:53:23 +0100

    PEC 1: Add icons to the menu using Feather dependency

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
+// Setup Feather icons
+import feather from "feather-icons";
+feather.replace();
 
 // Mark as active the current page in the navbar
 const links = document.querySelectorAll("nav ul a");
 links.forEach((link) => {
-    if (link.href === window.location.href) {
-        link.classList.add("active");
-    }
+  if (link.href === window.location.href) {
+    link.classList.add("active");
+  }
 });
